b28a235dc35b2fc1d86ffca199ede875bd5a1493
Adam Gluck <adam.gluck10@gmail.com>
2021-12-01 20:37:21 -0500

    updates

--- main.cpp
+++ main.cpp
@@ -14,27 +14,67 @@ class Button
         float x, y, width, height;
         std::string name;
     public:
-        Button(float, float, float, float, std::string);
+        Button(float x, float y, float width, float height, std::string name);
         bool isPressed(float, float);
         void draw();
 };
 
-class StartScreen
+
+class Screen
+{
+    public:
+        Screen(Button prevButton);
+        Button prevButton;
+        Button backButton= Button(0,0,100,20,"Back");
+        void draw();
+};
+
+class StartScreen: public Screen
 {
     public:
         StartScreen();
-        Button playButton;
-        Button statButton;
-        Button ruleButton;
-        Button creditButton;
+        Screen play;
+        Screen stat;
+        Screen rule;
+        Screen credit;
         void draw();
 };
+
+
+        
+
 
 int main() {
 
     StartScreen start = StartScreen();
+   
+
+
     start.draw();
-    
+    float x,y;
+    while(true)
+    {
+        while(!LCD.Touch(&x, &y)){}
+        if(start.play.prevButton.isPressed(x,y))
+        {
+            start.play.draw();
+        }
+        else if(start.stat.prevButton.isPressed(x,y))
+        {
+            start.stat.draw();
+        }
+        else if(start.rule.prevButton.isPressed(x,y))
+        {
+            start.rule.draw();
+        }
+        else if(start.credit.prevButton.isPressed(x,y))
+        {
+            start.credit.draw();
+        }
+       
+        
+
+    }
     //Never Closes
     while(true)
     {
@@ -59,7 +99,7 @@ void Button::draw()
     LCD.SetFontColor(BLUE);
     LCD.FillRectangle(x, y, width, height);
     LCD.SetFontColor(WHITE);
-    LCD.WriteAt(name, x+1, y+2);
+    LCD.WriteAt(name.c_str(), x+1, y+2);
 }
 
 bool Button::isPressed(float x, float y)
@@ -74,19 +114,45 @@ bool Button::isPressed(float x, float y)
     }
 }
 
-void StartScreen::StartScreen()
+Screen::Screen(Button prevButton)
 {
-    
-    this-> playButton = Button(0,100,100,20,"Play");
-    this-> statButton= Button(0,200,100,20,"Stats");
-    this-> ruleButton= Button(200,100,100,20,"Rules");
-    this-> creditButton= Button(200,200,100,20,"Credits");
+    this->prevButton = prevButton;
 }
+
+StartScreen::StartScreen()
+{
+    Button playButton = Button(0,100,100,20,"Play");
+    this->play = Screen(playButton);
+
+    Button statButton = Button(0,200,100,20,"Stats");
+    this->stat = Screen(statButton);
+
+    Button ruleButton = Button(200,100,100,20,"Rules");
+    this->rule = Screen(ruleButton);
+
+    Button creditButton = Button(200,200,100,20,"Credits");
+    this->credit = Screen(creditButton);
+    
+}
+
 
 void StartScreen::draw()
 {
-    playButton.draw();
-    statButton.draw();
-    ruleButton.draw();
-    creditButton.draw();
+    LCD.SetBackgroundColor(RED);
+    LCD.Clear();
+    LCD.WriteAt("Welcome to FEH Adventure",0,0);
+
+    play.draw();
+    stat.draw();
+    rule.draw();
+    credit.draw();
+
+}
+
+void Screen::draw()
+{
+    LCD.SetBackgroundColor(BLUE);
+    LCD.Clear();
+
+    backButton.draw();
 }
