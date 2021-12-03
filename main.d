d2dea6330faa73c3b658cc328cf77e3fee721d93
Adam G <37425558+aglucky@users.noreply.github.com>
2021-12-03 17:46:50 -0500

    wd

--- main.d
+++ main.d
@@ -1,12 +1,6 @@
 main.o: main.cpp simulator_libraries/FEHLCD.h \
  simulator_libraries/LCDColors.h simulator_libraries/FEhUtility.h \
-<<<<<<< HEAD
- mainmenu.h usertools.h Game.h Platform.h GameObject.h \
- C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\FEHLCD.h \
- C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\FEHUtility.h
-=======
  mainmenu.h usertools.h player.h
->>>>>>> main
 
 simulator_libraries/FEHLCD.h:
 
@@ -18,16 +12,4 @@ mainmenu.h:
 
 usertools.h:
 
-<<<<<<< HEAD
-Game.h:
-
-Platform.h:
-
-GameObject.h:
-
-C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\FEHLCD.h:
-
-C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\FEHUtility.h:
-=======
 player.h:
->>>>>>> main
