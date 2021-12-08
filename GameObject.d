1658121158a1e643c8f30ecf1386b1c6ce9abf39
Adam G <37425558+aglucky@users.noreply.github.com>
2021-12-08 09:52:43 -0500

    file stuff

--- GameObject.d
+++ GameObject.d
@@ -1,17 +1,13 @@
-GameObject.o: GameObject.cpp GameObject.h \
- C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\FEHLCD.h \
- C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\LCDColors.h \
- C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\FEHUtility.h \
- simulator_libraries/FEHImages.h simulator_libraries/FEHLCD.h
+GameObject.o: GameObject.cpp GameObject.h simulator_libraries/FEHLCD.h \
+ simulator_libraries/LCDColors.h simulator_libraries/FEHUtility.h \
+ simulator_libraries/FEHImages.h
 
 GameObject.h:
 
-C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\FEHLCD.h:
+simulator_libraries/FEHLCD.h:
 
-C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\LCDColors.h:
+simulator_libraries/LCDColors.h:
 
-C:\Users\happy\Documents\FEH\FEH_SDP\FEH_SDP\simulator_libraries\FEHUtility.h:
+simulator_libraries/FEHUtility.h:
 
 simulator_libraries/FEHImages.h:
-
-simulator_libraries/FEHLCD.h:
